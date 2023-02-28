4a85f267bdb4f28b60312a777561da2c9ed6f136
Abderrahman El asri <ael-asri@e2r3p9.1337.ma>
2023-02-28 19:22:55 +0100

    wp core && cli

---
+++ srcs/requirements/wordpress/conf/wp-conf.php
@@ -0,0 +1,7 @@
+define('DB_NAME', 'wp');
+
+define('DB_USER', 'wp_user');
+
+define('DB_PASSWORD', 'pass');
+
+define('DB_HOST', 'mariadb')
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,7 +1,29 @@
+#! /bin/bash
+
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x wp-cli.phar
 
-sudo mv wp-cli.phar /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/wp
 
 wp --info
+
+wp core download --allow-root
+
+wp core --allow-root config --dbhost=host.db --dbname=prefix_db --dbuser=username --dbpass=password
+
+
+# chmod +x wp-config.php
+
+cat "define('DB_NAME', 'wp');
+
+define('DB_USER', 'wp_user');
+
+define('DB_PASSWORD', 'pass');
+
+define('DB_HOST', 'mariadb')" > wp-config.php
+
+wp core --allow-root install --url=yourwebsite.com --title="Your Blog Title" --admin_name=wordpress_admin --admin_password=pass --admin_email=you@example.com
+
+exec $@
