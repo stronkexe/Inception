00d018efe464db089830bc97714b3448b7298257
stronk-exe <ael-asri@student.1337.ma>
2023-03-10 19:03:46 +0100

    adminer~ && redis~

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -5,3 +5,16 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core install --allow-root --url="ael-asri.42.fr" --title="inception" --admin_name="stronk" --admin_password="pass" --admin_email="stronk@me.com"
+
+wp plugin install redis-cache --activate --allow-root
+
+wp plugin update --all --allow-root
+
+wp theme install twentytwentytwo --activate --allow-root
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+mkdir /run/php
+wp config set WP_REDIS_HOST redis --allow-root
+
+wp redis enable --allow-root
